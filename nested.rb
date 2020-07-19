1efa1e729703cf541c8de92f25b174c6c2c3cbf4
jade decordova <lacroixjaded@gmail.com>
2020-07-19 03:15:32 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -16,7 +16,7 @@ def hopper
       }
     }
     
-   programmer_hash[:grace_hopper][:k]
+   programmer_hash[:grace_hopper]
 
 end
 
@@ -38,7 +38,7 @@ def alan_kay_is_known_for
         :languages => ["C"]
       }
     }
-
+    programmer_hash[alan_kay][:known_for]
 end
 
 def dennis_ritchies_language
